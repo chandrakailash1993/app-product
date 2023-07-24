9cb476a2027e7a7fb58024701fe7045b444f083a
Kailash Chandra <chandrakailash1993@gmail.com>
2023-07-24 19:25:54 +0530

    Added dynamic config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,26 @@ const NextFederationPlugin = require("@module-federation/nextjs-mf");
 // loading remotes on demand, not ideal for SSR
 const remotes = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
+
   return {
-    home: `home@${process.env.NEXT_HOME_APP_ENDPOINT}/_next/static/${location}/remoteEntry.js`,
-    shell: `shell@${process.env.NEXT_SHELL_APP_ENDPOINT}/_next/static/${location}/remoteEntry.js`,
+    home: `home@${process.env.NEXT_PUBLIC_HOME_APP_ENDPOINT}/_next/static/${location}/remoteEntry.js`,
+    shell: `shell@${process.env.NEXT_PUBLIC_SHELL_APP_ENDPOINT}/_next/static/${location}/remoteEntry.js`,
   };
 };
 
 const nextConfig = {
+  serverRuntimeConfig: {
+    NEXT_PUBLIC_HOME_APP_ENDPOINT: process.env.NEXT_PUBLIC_HOME_APP_ENDPOINT,
+    NEXT_PUBLIC_SHELL_APP_ENDPOINT: process.env.NEXT_PUBLIC_SHELL_APP_ENDPOINT,
+    NEXT_PUBLIC_PRODUCT_APP_ENDPOINT:
+      process.env.NEXT_PUBLIC_PRODUCT_APP_ENDPOINT,
+  },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_HOME_APP_ENDPOINT: process.env.NEXT_PUBLIC_HOME_APP_ENDPOINT,
+    NEXT_PUBLIC_SHELL_APP_ENDPOINT: process.env.NEXT_PUBLIC_SHELL_APP_ENDPOINT,
+    NEXT_PUBLIC_PRODUCT_APP_ENDPOINT:
+      process.env.NEXT_PUBLIC_PRODUCT_APP_ENDPOINT,
+  },
   webpack(config, options) {
     config.plugins.push(
       new NextFederationPlugin({
